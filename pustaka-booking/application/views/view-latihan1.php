705899a3bceb1ea310c0cab7a5f65afd58f8317d
syahienachen <syahienaputra313@gmail.com>
2022-04-05 06:16:21 +0700

    Menggunakan Controller, View dan Model

---
+++ pustaka-booking/application/views/view-latihan1.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>Latihan 1</title>
+    </head>
+    <body>
+        Halo kawan, yuk kita belajar web programming!<br>
+        Nilai ke-1 = <?=$nilai1;?>
+        Nilai ke-2 = <?=$nilai2;?>
+        ini hasil dari pemodelan dengan metode pejumlahan yaitu <?=$nilai1." + ".$nilai2." = ".$hasil;?>
+    </body>
+</html>
